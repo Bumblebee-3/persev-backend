cc15cd206c6ec31b341a822e687f158539b8fcc3
Bumblebee-3 <your.email@example.com>
2025-07-29 09:06:28 +0530

    idk

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# persev-backend
-Persev backend
+<div align="center">
+
+# Website
+Code to the official website for Perseverantia, 2025.
+
+
+
+https://github.com/user-attachments/assets/a459d288-3105-451a-a152-b6129d036bb1
+
+
+
+[https://perseverantia.events-at-scottishmahim.com/](https://perseverantia.events-at-scottishmahim.com/)
+</div>
+
+
+
---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = 3000;
+
+app.use(require("express").json())    // <==== parse request body as JSON
+      const bodyParser = require('body-parser');
+      app.use(bodyParser.urlencoded({ extended: false }));
+      app.use(bodyParser.json())
+      app.use(require("express-session")({
+        secret: require('crypto').randomBytes(16).toString("hex"),
+        cookie: { maxAge: thirtyDays },
+        resave: true,
+        saveUninitialized: true
+      }))
+
+
+// Load environment variables from .env file
+dotenv.config();
+
+const unames = process.env.UNAMES.split(',');
+const passwords = process.env.PASSWORDS.split(',');
+
+// Serve static files from the "assets" folder
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+
+function isLoggedIn(req,res,next){
+        for(let i=0;i<unames.length;i++){
+            if(unames[i]==req.session.username && passwords[i]==req.session.password){
+                return next();
+            }
+        }
+        res.redirect("/");
+}
+
+app.post("/pass/validate/",async (req,res)=>{
+        let uname= req.body.username;
+        let pass = req.body.password;
+        for(let i=0;i<data.accounts.length;i++){
+            if(data.accounts[i].username==uname && data.accounts[i].password==pass){
+                return res.json({data:true})
+            }
+        }
+        return res.json({data:false})
+    })
+
+    app.get('/login',  (req, res) => {
+      req.session.username = req.query.username;
+      req.session.password = req.query.password;
+      res.redirect("/panel")
+    })
+    app.get('/logout',  (req, res) => {
+      req.session.username = "";
+      req.session.password = "";
+      res.redirect("/?loggedout=true")
+    })
+    app.get("/panel",isLoggedIn,(req,res)=>{
+        const data = fs.readFileSync(__dirname+"/views/panel.html").toString()
+        const auth = req.session.username+"-"+req.session.password
+        res.send(data.replace(/(!auth)/g,auth));
+    })
+app.get('/events', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'events.html'));
+});
+
+app.get('/organizing-committee', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'organizing-committee.html'));
+});
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'style.css'));
+});
+
+app.get('/style2.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'style2.css'));
+});
+
+app.get('/config.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'config.json'));
+});
+// Route to serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+// Catch-all route for undefined paths to serve 404.html
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'static', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
